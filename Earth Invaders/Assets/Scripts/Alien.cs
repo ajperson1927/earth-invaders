80c1eeb791a32e88ae9f855f594f0f5436cb2c24
pontiaj2066 <pontiaj2066@gmail.com>
2020-05-22 20:22:12 -0600

    Player can now be selected and moved

---
+++ Earth Invaders/Assets/Scripts/Alien.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alien : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 2f;
+    [SerializeField] private float segmentsPerUnityUnit = 2f;
+    [SerializeField] private float padding = 0.5f;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (!GetComponent<PlayerControl>())
+        {
+            PlayerControl playerControl = gameObject.AddComponent<PlayerControl>();
+            playerControl.SetMovementSpeed(movementSpeed);
+            playerControl.SetSegmentsPerUnityUnit(segmentsPerUnityUnit);
+            playerControl.SetPadding(padding);
+        }
+    }
+}
---
+++ Earth Invaders/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerControl : MonoBehaviour
+{
+    private float movementSpeed = 2f;
+    private float segmentsPerUnityUnit = 2f;
+    private float padding = 0.5f;
+    private float xPos;
+    private float yPos;
+
+    private float xMin;
+    private float xMax;
+    private float yMin;
+    private float yMax;
+
+    public void SetMovementSpeed(float speed)
+    {
+        movementSpeed = speed;
+    }
+
+    public void SetSegmentsPerUnityUnit(float segments)
+    {
+        segmentsPerUnityUnit = segments;
+    }
+
+    public void SetPadding(float padding)
+    {
+        this.padding = padding;
+    }
+    void Start()
+    {
+        transform.parent = null;
+        xPos = transform.position.x;
+        yPos = transform.position.y;
+        SetupMoveBoundaries();
+        GetComponent<SpriteRenderer>().color = Color.green;
+    }
+    
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float inputX = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
+        float inputY = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+
+        xPos = Mathf.Clamp(xPos + inputX, xMin, xMax);
+        yPos = Mathf.Clamp(yPos + inputY, yMin, yMax);
+
+        
+
+        var newX = Mathf.RoundToInt(xPos * segmentsPerUnityUnit) / segmentsPerUnityUnit;
+        var newY = Mathf.RoundToInt(yPos * segmentsPerUnityUnit) / segmentsPerUnityUnit;
+        
+        transform.position = new Vector2(newX, newY); 
+
+    }
+
+    private void SetupMoveBoundaries()
+    {
+        Camera camera = Camera.main;
+        xMin = camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = camera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = camera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+}
